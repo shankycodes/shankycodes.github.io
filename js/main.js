f7df2552ac6066dab478aaec2c66fe4fa2799a59
shankycodes <138747047+shankycodes@users.noreply.github.com>
2023-09-09 12:28:32 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var menuBtn = document.querySelector('.menu-btn');
+var hamburger = document.querySelector('.menu-btn__hamburger');
+
+var nav = document.querySelector('.nav');
+var menuNav = document.querySelector('.nav-menu');
+var navItems = document.querySelectorAll('.nav-menu__item');
+
+
+var showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+
+    if(!showMenu){
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
+
+        showMenu = true;
+    }else{
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
+
+        showMenu = false;
+    }
+}
